bc13ad986a079a93effbb68607b6164b33995ad1
Mohamed Elsayed <64938275+mohamed-elsayed13@users.noreply.github.com>
2025-01-03 02:29:03 +0200

    Update MipsTestbench.vhd

--- MIPS/MipsTestbench.vhd
+++ MIPS/MipsTestbench.vhd
@@ -58,49 +58,4 @@ begin
 end Testbench;
 
 
--- -- Testbench for MIPS Processor
 
--- library IEEE;
--- use IEEE.STD_LOGIC_1164.ALL;
--- use IEEE.NUMERIC_STD.ALL;
-
--- -- Entity Declaration
--- entity MIPSProcessor_tb is
--- end MIPSProcessor_tb;
-
--- -- Architecture Declaration
--- architecture Testbench of MIPSProcessor_tb is
-    -- -- Component declaration of the MIPS Processor
-    -- component MIPSProcessor
-        -- port(
-            -- clk : in STD_LOGIC
-        -- );
-    -- end component;
-
-    -- -- Signals
-    -- signal clk : STD_LOGIC := '0';
-
-    -- -- Clock period constant
-    -- constant clk_period : time := 10 ns;
-
--- begin
-
-    -- -- Instantiate the Unit Under Test (UUT)
-    -- uut: MIPSProcessor
-        -- port map(
-            -- clk => clk
-        -- );
-
-    -- -- Clock generation process
-    -- clock_process : process
-    -- begin
-        -- while true loop
-            -- clk <= '0';
-            -- wait for clk_period / 2;
-            -- clk <= '1';
-            -- wait for clk_period / 2;
-        -- end loop;
-    -- end process;
-
-
--- end Testbench;
